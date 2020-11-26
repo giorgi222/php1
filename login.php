746dcdee3012a42fdfe1397bf965e6090bf49851
giorgi222 <75039075+giorgi222@users.noreply.github.com>
2020-11-26 12:09:18 -0800

    Create login.php

---
+++ login.php
@@ -0,0 +1,22 @@
+<?php
+$name =$_POST['name'];
+$lastname=$_POST['lastname'];
+$paroli=$_POST['paroli'];
+$user->name=$name;
+$user->lastname=$lastname;
+$user->name=$name;
+$myfile = fopen("userlist.txt", "r+") or die("Unable to open file!");
+$userlist = json_decode($myfile);
+$j=0;
+foreach ($userlist as $user=> $value) {
+	j++;
+	if($user==this $user){
+<script type="text/javascript">location.href = '0.0.0.0';</script>
+
+echo "გამარჯობა ".$name ." ". $lastname ;
+
+
+	}
+	elseif(j==$userlist.length){<script type="text/javascript">alert("მომხმარებელი არ მოიძებნა დაბრუნდით საიტზე და თავიდან გაიარეთ რეგისტრაცია")</script>};
+}
+?>
